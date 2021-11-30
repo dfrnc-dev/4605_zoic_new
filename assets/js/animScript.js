999de98c696bf9a6e9cc8b7f328ea9827a75f87a
dfrnc-dev <login.dfrnc@gmail.com>
2021-11-30 16:41:00 +0200

    fix y:150%

--- assets/js/animScript.js
+++ assets/js/animScript.js
@@ -221,7 +221,7 @@ const anim = function(){
                 .set(`${nameScene}`, { autoAlpha: 1 }, "<")
                 .add("qq", "<")
                 .to(".main_animation_container", { duration: 1, background: temBgColor, ease: "sine.inOut" }, "qq")
-                .from(`${nameScene} .moveElements > *`, { duration: 1, autoAlpha: 0, y: "100%", ease: "sine.out", stagger: { each: 0.05, from: "end" } }, "qq")
+                .from(`${nameScene} .moveElements > *`, { duration: 1, autoAlpha: 0, y: "150%", ease: "sine.out", stagger: { each: 0.05, from: "end" } }, "qq")
                 .from(`${nameScene} .lineBG > * > *`, { duration: 1, drawSVG: 0, ease: "sine.inOut", stagger: { each: 0.2, from: "end" } }, "qq+=1")
                 .from(`${nameScene} .elLine`, { duration: 1, drawSVG: 0, ease: "sine.out", stagger: { each: 0.02, from: "end" } }, "qq+=1.5")
                 .from(`${nameScene} .elCircle`, { duration: 1, scale: 0, transformOrigin: "50% 50%", ease: "sine.out", stagger: { each: 0.05, from: "end" } }, "qq+=1")
@@ -231,7 +231,7 @@ const anim = function(){
 
         if (typeAnimation == 'translate-up-out') {
             return gsap.timeline({ paused: true })
-                .to(`${nameScene} .moveElements > *`, { duration: 2, y: "-100%", transformOrigin: "50% 50%", ease: "power4.inOut", stagger: { each: 0.08, from: "end" } }, )
+                .to(`${nameScene} .moveElements > *`, { duration: 2, y: "-150%", transformOrigin: "50% 50%", ease: "power4.inOut", stagger: { each: 0.08, from: "end" } }, )
                 .to(`${nameScene} .lineBG > *`, { duration: 1, autoAlpha: 0, ease: "power4.inOut", stagger: { each: 0.2, from: "end" } }, "<")
                 // .to(`${nameScene} .moveElements > *`, { duration: 0.2, autoAlpha: 0, ease: "none", stagger: { each: 0.1, from: "end" } }, "<+4")
                 .set(`${nameScene}`, { autoAlpha: 0 }, ">")
@@ -342,7 +342,7 @@ const anim = function(){
                     .add(animationScene('.secAnim-andThoseChoices', 'scale-up-in', 5, 0.9).restart(), `<`)
                     .add(animationScene('.secAnim-andThoseChoices', 'translate-up-out', 2, 0.9).restart(), '>')
 
-                    .add(animationScene('.secAnim-2455inRewards', 'translate-up-in', 3.7, 0.9).restart(), `<`)
+                    .add(animationScene('.secAnim-2455inRewards', 'translate-up-in', 3.7, 0.9).restart(), `<+0.2`)
                     .add(animationScene('.secAnim-2455inRewards', 'translate-right-out', 1.5, 0.9).restart(), '>')
 
                     .add(animationScene('.secAnim-745Balance', 'translate-right-in', 3.7, 0.9).restart(), `<`)
@@ -455,7 +455,7 @@ const anim = function(){
                     .add(animationScene('.secAnim-andThoseChoices', 'scale-up-in', 5, 0.9).restart(), `<`)
                     .add(animationScene('.secAnim-andThoseChoices', 'translate-up-out', 2, 0.9).restart(), '>')
 
-                    .add(animationScene('.secAnim-1250inRewards', 'translate-up-in', 3.7, 0.9).restart(), `<`)
+                    .add(animationScene('.secAnim-1250inRewards', 'translate-up-in', 3.7, 0.9).restart(), `<+0.2`)
                     .add(animationScene('.secAnim-1250inRewards', 'scale-down-out', 1.5, 0.9).restart(), '>')
 
                     .add(animationScene('.secAnim-2022Season2', 'scale-down-in', 5.5, 1.1).restart(), `<`)
